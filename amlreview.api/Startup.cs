565fd67db5960a957ba7b0f7dada465808f433ed
Khayyam Mammadov <khayyam.mammadov@visma.com>
2021-09-20 09:07:08 +0300

    Added CORS

--- amlreview.api/Startup.cs
+++ amlreview.api/Startup.cs
@@ -18,10 +18,23 @@ namespace amlreview.api
         }
 
         public IConfiguration Configuration { get; }
-
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:53135",
+                                        "http://localhost:4200"
+                                        )
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod();
+                });
+            });
+
             services.AddDbContext<SchedulerContext>(options => options.UseInMemoryDatabase(databaseName: "AMLReviewDb"));
 
             services.AddScoped<ICompanyRepository, CompanyRepository>();
@@ -43,6 +56,8 @@ namespace amlreview.api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(MyAllowSpecificOrigins);
+
             app.UseRouting();
 
            // app.UseAuthorization();
@@ -51,6 +66,7 @@ namespace amlreview.api
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
